002a26c060d020e895f549ae3db503fcb3f936d1
unknown <tromavoto@abv.bg>
2019-04-18 19:13:46 +0300

    Updated

---
+++ src/Forum.App/Commands/SubmitCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace Forum.App.Commands
+{
+    using System;
+
+    using Contracts;
+
+    public class SubmitCommand : ICommand
+    {
+        private ISession session;
+        private IPostService postService;
+
+        public SubmitCommand(ISession session, IPostService postService)
+        {
+            this.session = session;
+            this.postService = postService;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            string replyContent = args[0];
+
+            if (string.IsNullOrEmpty(replyContent))
+            {
+                throw new ArgumentException("Reply cannot have empty content!");
+            }
+
+            int postId = int.Parse(args[1]);
+
+            int authorId = this.session.UserId;
+
+            this.postService.AddReplyToPost(postId, replyContent, authorId);
+
+            return this.session.Back();
+        }
+    }
+}
---
+++ src/Forum.App/ViewModels/ContentViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Forum.App.ViewModels
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public abstract class ContentViewModel
+    {
+        private const int lineLength = 37;
+
+        protected ContentViewModel(string content)
+        {
+            this.Content = this.GetLines(content);
+        }
+
+        public string[] Content { get; }
+
+        private string[] GetLines(string content)
+        {
+            char[] contentChars = content.ToCharArray();
+
+            ICollection<string> lines = new List<string>();
+
+            for (int i = 0; i < content.Length; i += lineLength)
+            {
+                char[] row = contentChars.Skip(i).Take(lineLength).ToArray();
+
+                string rowString = string.Join("", row);
+
+                lines.Add(rowString);
+            }
+
+            return lines.ToArray();
+        }
+    }
+}
